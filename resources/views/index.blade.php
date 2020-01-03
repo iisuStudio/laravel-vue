e5bef18f2e81127ca7cf49f99b81fb41de7cf5b2
steven <steven@henghung.com>
2020-01-03 15:21:29 +0800

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Icon -->
+        <link rel="icon" href="{{ asset('favicon.ico') }}">
+
+        <!-- Style -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <noscript>
+            <strong>We're sorry but vue doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+        </noscript>
+        <div id="app"></div>
+        <!-- built files will be auto injected -->
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
